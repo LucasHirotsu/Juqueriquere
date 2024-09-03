1f1ed7db7317fd6fa9db7f1a7c3480946b66606d
Ygor <99401550+FatalRestart@users.noreply.github.com>
2024-09-03 09:48:49 -0300

    Update cadastrotrilha.js
    adicionado função de editar trilhas

--- Juqueriquere/frontend/js/cadastrotrilha.js
+++ Juqueriquere/frontend/js/cadastrotrilha.js
@@ -1,5 +1,6 @@
 function cancel(){
-    document.getElementById('trilhaModal').style.display = 'none';
+    document.getElementById('trilhaModalEdit').style.display = 'none';
+    document.getElementById('trilhaModalDelete').style.display = 'none';
   };
 
 
@@ -12,14 +13,14 @@ async function deletarTrilhaConfirm(id) {
         const response = await fetch(`http://localhost:9000/trilhas/${id}`);
         const trilhajson = await response.json([0]);
         const trilha = trilhajson[0]
-        document.getElementById('trilhaModal').style.display = 'block';
+        document.getElementById('trilhaModalDelete').style.display = 'block';
         document.getElementById('trilhaNome').innerText = trilha.nome;
         document.getElementById('buttonExcluir').addEventListener('click', () => deletarTrilha(id));
         } catch (error) {
         console.error('Erro:', error);
         alert('Erro ao deletar trilha');
         }
-        }
+};
 async function deletarTrilha(id) {
     
     try {
@@ -37,7 +38,79 @@ async function deletarTrilha(id) {
           console.error('Erro:', error);
           alert('Erro ao deletar trilha');
         }
-      }
+};
+
+async function editarTrilha(id){
+  try{
+    const response = await fetch(`http://localhost:9000/trilhas/${id}`);
+    const trilhajson = await response.json();
+    const trilha = trilhajson[0]
+    var items = ['nome', 'local', 'extensao', 'altitude', 'duracao', 'dificuldade', 'monitoria', 'agendamento', 'caracteristicas', 'descricao', 'horario', 'imagem', 'destaque'];
+    items.forEach(item => {
+      console.log(item)
+      entries = Object.entries(trilha)
+      entries.map( ([key, val] = entry) => {
+        if (item == key){
+          return document.getElementById(item).placeholder = val, document.getElementById(item).checked = val;
+        }
+      });
+      
+
+
+    });
+
+    
+    document.getElementById('buttonEdit').addEventListener('click', () => editarTrilhaConfirm(id));
+    document.getElementById('trilhaModalEdit').style.display = 'block';
+  }catch(error){
+    console.error('Erro: ', error);
+    alert('Erro ao carregar detalhes da trilha')
+  }
+
+
+  
+  
+
+  
+};
+
+async function editarTrilhaConfirm(id){
+  const trilha = {
+    nome: document.getElementById('nome').value,
+    local: document.getElementById('local').value,
+    extensao: document.getElementById('extensao').value,
+    altitude: document.getElementById('altitude').value,
+    duracao: document.getElementById('duracao').value,
+    dificuldade: document.getElementById('dificuldade').value,
+    monitoria: document.getElementById('monitoria').checked ? 1 : 0,
+    agendamento: document.getElementById('agendamento').checked ? 1 : 0,
+    caracteristicas: document.getElementById('caracteristicas').value,
+    descricao: document.getElementById('descricao').value,
+    horario: document.getElementById('horario').value,
+    imagem: document.getElementById('imagem').value,
+    destaque: document.getElementById('destaque').checked ? 1 : 0 
+};
+  try {
+      const response = await fetch('http://localhost:9000/trilhas/' + id, {
+        method: 'PUT',
+        headers:{
+          'Content-Type': 'application/json'
+        },body: JSON.stringify(trilha)
+      });
+      
+      const result = await response.json();
+    if (response.ok) {
+      alert(result.mensagem);
+      location.reload();
+    } else {
+      alert(result.mensagem || 'Trilha editada com sucesso!');
+    }
+  } catch (error) {
+    console.error('Erro:', error);
+    alert('Erro ao editar trilha?');
+  }
+};
+  
 
 async function fetchTrilhas() {
     try {
@@ -101,8 +174,8 @@ async function fetchTrilhas() {
                     <br>
                     <p><span>Acesso:</span> ${trilha.agendamento ? 'Agendamento necessário' : 'Gratuito'}</p>
                     <div class="card-buttons">
-                      <button class="delete"><span class="material-symbols-outlined " onclick="deletarTrilhaConfirm(${trilha.idtrilhas})">delete</span></button>
-                      <button class="edit"><span class="material-symbols-outlined ">edit</span></button>
+                      <button class="delete" onclick="deletarTrilhaConfirm(${trilha.idtrilhas})"><span class="material-symbols-outlined ">delete</span></button>
+                      <button class="edit" onclick="editarTrilha(${trilha.idtrilhas})"><span class="material-symbols-outlined ">edit</span></button>
                     </div>
                     
                 
@@ -113,7 +186,7 @@ async function fetchTrilhas() {
     } catch (error) {
         console.error('Erro ao buscar trilhas:', error);
     }
-}
+};
 
 document.addEventListener('DOMContentLoaded', fetchTrilhas);
 
